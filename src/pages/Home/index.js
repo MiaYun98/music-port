01c8a03504d12732928b7d141a3c0584228ed4dd
Mia <113086524+MiaYun98@users.noreply.github.com>
2022-12-22 22:43:25 -0800

    click effect

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,13 +8,13 @@ export default function Home() {
 
   useEffect(() => {
     console.log(play)
-
+    console.log(titleSong)
   }, [play])
   
   const musichandle = (e) => {
-    console.log("click")
     console.log(e.target)
     if(play === false) {
+      setTitleSong(e.target.getAttribute('musiTitle'))
       setPlay(true)
     } else if (play === true) {
       setPlay(false)
@@ -24,23 +24,23 @@ export default function Home() {
   return (
     <div className="home container">
       <ul className='music'>
+        <li className='card' onClick={musichandle} musiTitle = 'Pristellium'>
+          <img src={FirstSong} alt='Pristellium'/>
+          <p>Pristellium</p>
+        </li>
         <li className='card' onClick={musichandle}>
           <img src={FirstSong}/>
           <p>Pristellium</p>
         </li>
-        <li className='card'>
+        <li className='card' onClick={musichandle}>
           <img src={FirstSong}/>
           <p>Pristellium</p>
         </li>
-        <li className='card'>
+        <li className='card' onClick={musichandle}>
           <img src={FirstSong}/>
           <p>Pristellium</p>
         </li>
-        <li className='card'>
-          <img src={FirstSong}/>
-          <p>Pristellium</p>
-        </li>
-        <li className='card'>
+        <li className='card' onClick={musichandle}>
           <img src={FirstSong}/>
           <p>Pristellium</p>
         </li>
