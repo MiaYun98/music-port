0bb4f82ec4b7e987592c4dd425899f840c02ba97
Mia <113086524+MiaYun98@users.noreply.github.com>
2023-02-17 15:09:11 -0800

    add

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -4,7 +4,7 @@ import "./style.css"
 export default function Contact () {
     return (
         <div className="contact">
-            <h1>contact information</h1>
+            <h1>If you have any question Contact through: </h1>
             <p>SoundCloud</p>
         </div>
     )
