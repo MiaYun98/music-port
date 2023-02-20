a72947bf0cbe3eaba0ab08d917fe16101bff1d4d
Mia <113086524+MiaYun98@users.noreply.github.com>
2023-02-20 12:14:52 -0800

    About

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,15 +1,16 @@
-import {React} from "react"
+import { React } from "react"
 import "./style.css"
 
-export default function About () {
+export default function About() {
     return (
         <div className="about">
             <h1>Chris Han</h1>
-            <h2>Education</h2>
-            <p>University of Washington</p>
-            <h2>Work Place</h2>
-            <p>Sound Cloud / Twitter</p>
-            
+            <div className="about-detail">
+                <h2>Education</h2>
+                <p>University of Washington</p>
+                <h2>Work Place</h2>
+                <p>Sound Cloud / Twitter</p>
+            </div>
         </div>
     )
 }
