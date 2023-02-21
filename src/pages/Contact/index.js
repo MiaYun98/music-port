30252b94264bf743ef0b8be8e2454d30cff18b72
Mia <113086524+MiaYun98@users.noreply.github.com>
2023-02-21 10:30:04 -0800

    linking

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -5,8 +5,9 @@ export default function Contact () {
     return (
         <div className="contact">
             <h1>Contact Information </h1>
-            <p>SoundCloud</p>
-            <p>SoundCloud</p>
+            {/* should be in a a tag that can direct to the link */}
+            <a><p>SoundCloud</p></a>
+            <p>Gamail</p>
             <p>SoundCloud</p>
             <p>SoundCloud</p>
         </div>
